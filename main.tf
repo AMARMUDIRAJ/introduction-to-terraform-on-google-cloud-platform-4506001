edc22ca515e0fc2e2ec23aef1bb8080999730536
AMARMUDIRAJ <69884578+AMARMUDIRAJ@users.noreply.github.com>
2024-07-25 17:03:21 +0530

    stagging and prod environment creation form QA

--- main.tf
+++ main.tf
@@ -9,3 +9,29 @@ app_name        = "qa-blog"
 networks_name   = "qa"
 
 }
+
+
+module "stagging" {
+source = "./modules/blog"
+
+project_id = var.project_id
+region = var.region
+
+
+app_name        = "stagging-blog"
+networks_name   = "stagging"
+
+}
+
+
+module "prod" {
+source = "./modules/blog"
+
+project_id = var.project_id
+region = var.region
+
+
+app_name        = "prod-blog"
+networks_name   = "prod"
+
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
 output "qa_ip" {
     value = module.qa.public_ip
 }
+
+output "stagging_ip" {
+    value = module.stagging.public_ip
+}
+
+
+output "prod_ip" {
+    value = module.prod.public_ip
+}
